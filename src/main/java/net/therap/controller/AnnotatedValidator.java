861e6d1f5861bf046a8f9a6a08c201c34f97e855
Sadia Afroz <sadia.afroz@sadia.afroz>
2021-03-25 15:39:28 +0600

    reviewed-1

--- src/main/java/net/therap/controller/AnnotatedValidator.java
+++ src/main/java/net/therap/controller/AnnotatedValidator.java
@@ -1,72 +1,42 @@
 package net.therap.controller;
 
-import net.therap.model.Person;
 import net.therap.model.Size;
 import net.therap.model.ValidationError;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author sadia.afroz
  * @since 3/24/21
  */
 public class AnnotatedValidator {
-    public static Pattern Annotation_Attribute_Match = Pattern.compile("\\{\\w+\\}");
 
-    public static void validate(Person p, List<ValidationError> errors) {
-        Class c = p.getClass();
+    public static void validate(Object object, List<ValidationError> errors) {
+        Class c = object.getClass();
         for (Field field : c.getDeclaredFields()) {
             field.setAccessible(true);
             if (field.isAnnotationPresent(Size.class)) {
                 Annotation annotation = field.getAnnotation(Size.class);
                 Size size = (Size) annotation;
-                String original = size.message();
-                Matcher matcher = Annotation_Attribute_Match.matcher(original);
-
-                if (field.getType().equals(Integer.TYPE)) {
-                    if (p.getAge() < size.min()) {
-
-                        int lastIndex = 0;
-                        StringBuilder output = new StringBuilder();
-                        while (matcher.find()) {
-                            output.append(original, lastIndex, matcher.start()).append(size.min());
-                            lastIndex = matcher.end();
-                        }
-                        if (lastIndex < original.length()) {
-                            output.append(original, lastIndex, original.length());
-                        }
-
-                        ValidationError validationError = new ValidationError(field.getName(), (String) field.getType().toString(), output.toString());
+                Object value = null;
+                try {
+                    value = field.get(object);
+                    int checkValue = 0;
+                    if (value.getClass().equals(Integer.class)) {
+                        checkValue = (Integer) value;
+                    } else if (value.getClass().equals(String.class)) {
+                        checkValue = ((String) value).length();
+                    }
+                    String outputMessage = size.message().replaceAll("\\{min\\}", Integer.toString(size.min())).replaceAll("\\{max\\}",
+                            Integer.toString(size.max()));
+                    if (checkValue <= size.min() || checkValue >= size.max()) {
+                        ValidationError validationError = new ValidationError(field.getName(), field.getType().getSimpleName(), outputMessage);
                         errors.add(validationError);
                     }
-                }
-                if (field.getType() == (String.class)) {
-                    if (p.getName().length() > size.max()) {
-
-                        int lastIndex = 0, count = 0;
-                        StringBuilder output = new StringBuilder();
-                        while (matcher.find()) {
-                            if (count == 0) {
-                                output.append(original, lastIndex, matcher.start())
-                                        .append(size.min());
-                            } else {
-                                output.append(original, lastIndex, matcher.start())
-                                        .append(size.max());
-                            }
-                            lastIndex = matcher.end();
-                            count++;
-                        }
-                        if (lastIndex < original.length()) {
-                            output.append(original, lastIndex, original.length());
-                        }
-
-                        ValidationError validationError = new ValidationError(field.getName(), (String) field.getType().getSimpleName(), output.toString());
-                        errors.add(validationError);
-                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
                 }
             }
         }
