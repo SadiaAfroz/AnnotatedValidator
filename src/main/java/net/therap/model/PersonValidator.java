63d442fab1eebbe9306332a8ea7d4235adf830a7
Sadia Afroz <sadia.afroz@sadia.afroz>
2021-03-28 15:52:57 +0600

    generic validator for custom object added

--- src/main/java/net/therap/model/PersonValidator.java
+++ src/main/java/net/therap/model/PersonValidator.java
@@ -4,7 +4,7 @@ package net.therap.model;
  * @author sadia.afroz
  * @since 3/28/21
  */
-public class PersonValidator implements SizeAnnotationClassVale<Person> {
+public class PersonValidator implements SizeAnnotationClassValue<Person> {
     @Override
     public int getSizeValue(Person person) {
         return person.getAge();
--- src/main/java/net/therap/model/SizeAnnotationClassValue.java
+++ src/main/java/net/therap/model/SizeAnnotationClassValue.java
@@ -4,6 +4,6 @@ package net.therap.model;
  * @author sadia.afroz
  * @since 3/28/21
  */
-public interface SizeAnnotationClassVale<T> {
+public interface SizeAnnotationClassValue<T> {
     int getSizeValue(T object);
 }
